c7a5dbe5be0ef8165acf209ba6434e5992fd3241
Dawid Plocki <dawid.plocki@gmail.com>
2024-01-22 20:04:40 +0100

    Add the http-server test example

--- e2e/test_example.py
+++ e2e/test_example.py
@@ -1,3 +1,6 @@
+import requests
+
+
 # content of test_sample.py
 def func(x):
     return x + 1
@@ -5,3 +8,10 @@ def func(x):
 
 def test_answer():
     assert func(3) == 4
+
+
+def test_query_params(httpserver):
+    httpserver.expect_request("/foo", query_string={"user": "user1", "group": "group1"}).respond_with_data("OK")
+
+    assert requests.get(httpserver.url_for("/foo?user=user1&group=group1")).status_code == 200
+    assert requests.get(httpserver.url_for("/foo?group=group1&user=user1")).status_code == 200
